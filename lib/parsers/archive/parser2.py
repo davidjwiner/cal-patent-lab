f08c903a83fd1942569f9d4f28eaef3547fd4e94
Dany <dany.srage@gmail.com>
2016-11-05 13:34:23 -0700

    add script to populate db

---
+++ lib/parsers/archive/parser2.py
@@ -0,0 +1,42 @@
+# this parser handles the new data about patents which were analyzed post-grant
+# Those documents are formatted differently, hence the need for a new parser
+
+import re
+import locale
+locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' ) 
+
+casefile_tmpl = "./output/RetrievePdf_{:03}.txt"
+propfile_tmpl = "./output/file_{:03}_properties.txt"
+
+def parsePatentId(fileIndex):
+	infile = casefile_tmpl.format(fileIndex)
+	outfile = propfile_tmpl.format(fileIndex)
+	matcher = re.compile('(?<=Patent )\s*[0-9,]+')
+	# If we find a mention of Patent followed by numbers w/commas, we find the ID
+	with open(infile) as f:
+		text = f.read().strip()
+		result = matcher.search(text)
+		if result:
+			#remove commas, write to properties file
+			result = result.group(0).replace(',','')
+			with open(outfile, 'a') as propertiesFile:
+				propertiesFile.write("PatentId: {}\n".format(result))
+
+def parseDecision(fileIndex):
+	infile = casefile_tmpl.format(fileIndex)
+	outfile = propfile_tmpl.format(fileIndex)
+	invalMatcher1 = re.compile('ORDERED.+?claim(s)?\s+\d+.+?(unpatentable|anticipated|cancelled)')
+	invalMatcher2 = re.compile('ORDERED.+?[aA]dverse\s+[jJ]udgment.+?claim(s)?\s+\d+.+?(granted|GRANTED)')
+	with open(infile) as f:
+		text = f.read().replace('\n', ' ')
+		result1 = invalMatcher1.search(text)
+		result2 = invalMatcher2.search(text)
+		with open(outfile, 'a') as propertiesFile:
+			if result1 or result2:
+				propertiesFile.write("Decision: invalidated\n")
+			else:
+				propertiesFile.write("Decision: not invalidated\n")
+
+for i in range(60):
+	parsePatentId(i)
+	parseDecision(i)
---
+++ lib/populate_db.py
@@ -0,0 +1,56 @@
+import MySQLdb
+import parsers.parser_new_data as parser
+
+MYSQL_HOST = "cal-patent-lab.chhaitskv8dz.us-west-2.rds.amazonaws.com"
+MYSQL_USERNAME = "***REMOVED***"
+MYSQL_PASSWORD = "***REMOVED***"
+MYSQL_DB = "***REMOVED***"
+
+def db(host, username, psswd, db):
+    db = MySQLdb.connect(host, username, psswd, db)
+    return db
+
+def insert_decision(cursor, table_name, patent_id, decision):
+    # SQL query to add a new row containing patent id and deciscion
+    # the invalidated field of the table takes an int as an input
+    if decision == "invalidated":
+        dec = 1
+    else:
+        dec = 0
+    sql_query = "INSERT INTO {} (patent_id, invalidated) VALUES ({}, {})".format(
+        table_name, str(patent_id), dec)
+
+    cursor.execute(sql_query)
+    return
+
+
+def update_table(db):
+    # statu contains false if the db was not updated
+    status = False
+    try:
+        db.commit()
+        status = True
+    except:
+        db.rollback()
+
+    # Closing the connection
+    db.close()
+    return status
+
+
+def populate_table_decision(db, table_name, patent_id, decision):
+    cursor = db.cursor()
+    insert_decision(cursor, table_name, patent_id, decision)
+    status = update_table(db)
+    return status
+
+
+if __name__ == "__main__":
+    print('Example')
+    # Example of file being parsed and the data stored in the db
+    # Please change the path adequatly for the ptab text file
+    patent_id=parser.parsePatentId("RetrievePdf_008.txt")
+    decision = parser.parseDecision("RetrievePdf_008.txt")
+    db = db(MYSQL_HOST, MYSQL_USERNAME, MYSQL_USERNAME, MYSQL_DB)
+    status = populate_table_decision(db, 'patents', patent_id, decision)
+    print(status)
