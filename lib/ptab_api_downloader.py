737c8ee81e6e2b8380e8f3ce82df6096473315c0
William Ho <william.zt.ho@berkeley.edu>
2017-01-14 10:38:30 -0800

    ptab_api_downloader.py: export all case data fields as JSON

--- lib/ptab_api_downloader.py
+++ lib/ptab_api_downloader.py
@@ -10,7 +10,7 @@ import time
 
 ptab_url_tmpl = "https://ptabdata.uspto.gov/ptab-api/trials?limit=100&offset={}"
 delay_mean = 4.2
-delay_stddev = delay_mean * 0.05
+delay_stddev = delay_mean * 0.07
 
 def get_url(url):
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
@@ -29,29 +29,31 @@ def get_num_cases():
 def main():
     argv = sys.argv[1:]
     if len(argv) != 1:
-        print("Usage: ptab_api_downloader.py path_to_output_file")
+        print("Usage: ptab_api_downloader.py path/to/output_file")
         sys.exit(1)
     
     out_filename = argv[0]
-    writer = csv.writer(open(out_filename, "w"))
+    out_fd = open(out_filename, "w")
     
     num_cases = get_num_cases()
+    cases = []
     counter = 0
     # Download information for up to 100 cases at a time
     for offset in range(0, num_cases, 100):
         ptab_url = ptab_url_tmpl.format(offset)
         response = get_url(ptab_url)
-        for case in response["results"]:
-            counter += 1
-            print("Processing case {}/{}".format(counter, num_cases))
-            case_no = case["trialNumber"]
-            patent_no = case["patentNumber"]
-            status = case["prosecutionStatus"]
-            writer.writerow((case_no, patent_no, status))
+        cases.extend(response["results"])
+        counter += len(response["results"])
+        print("Downloaded {} of {} cases".format(counter, num_cases))
         # Add random delay between consecutive requests
         delay = round(abs(random.gauss(delay_mean, delay_stddev)), 2)
         print("Sleeping {}s".format(delay))
         time.sleep(delay)
+    
+    # Write case information as JSON to output file
+    json.dump(cases, out_fd, indent=2)
+    out_fd.close()
+     
 
 if __name__ == "__main__":
     main()
