821ab03a38c7b9f8b02195b07df0b97401d8a8f1
William Ho <willh@chaos.ocf.berkeley.edu>
2017-01-27 21:34:45 -0800

    PTAB API downloader: fetch decision docs for cases with FWDs

--- lib/ptab_api_downloader.py
+++ lib/ptab_api_downloader.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
 
 import json
+from os import makedirs
+from os import path
 import random
 import urllib2
 import ssl
@@ -8,51 +10,139 @@ import sys
 import time
 
 ptab_url_tmpl = "https://ptabdata.uspto.gov/ptab-api/trials?limit=100&offset={}"
-delay_mean = 4.2
+delay_mean = 1.0
 delay_stddev = delay_mean * 0.07
 
-def get_url(url):
+def get_raw_contents_at(url):
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     response = urllib2.urlopen(url, context=ssl_context)
     contents = response.read()
-    decoded = json.loads(contents)
-    return decoded
+    return contents
+
+
+def get_json_at(url):
+    return json.loads(get_raw_contents_at(url))
 
 
 def get_num_cases():
     ptab_url = ptab_url_tmpl.format(0)
-    response = get_url(ptab_url)
+    response = get_json_at(ptab_url)
     return response["metadata"]["count"]
 
 
-def main():
-    argv = sys.argv[1:]
-    if len(argv) != 1:
-        print("Usage: ptab_api_downloader.py path/to/output_file")
-        sys.exit(1)
-    
-    out_filename = argv[0]
-    out_fd = open(out_filename, "w")
-    
+def random_sleep():
+    delay = round(abs(random.gauss(delay_mean, delay_stddev)), 2)
+    time.sleep(delay)
+
+
+# Test if any member of a collection of items is in another collection
+def contains(source, has):
+    for item in has:
+        if item in source:
+            return True
+    return False
+
+
+def get_case_metadata():
     num_cases = get_num_cases()
     cases = []
     counter = 0
     # Download information for up to 100 cases at a time
     for offset in range(0, num_cases, 100):
         ptab_url = ptab_url_tmpl.format(offset)
-        response = get_url(ptab_url)
+        response = get_json_at(ptab_url)
         cases.extend(response["results"])
         counter += len(response["results"])
         print("Downloaded {} of {} cases".format(counter, num_cases))
         # Add random delay between consecutive requests
-        delay = round(abs(random.gauss(delay_mean, delay_stddev)), 2)
-        print("Sleeping {}s".format(delay))
-        time.sleep(delay)
+        random_sleep()
+    return cases
+
+
+def download_fwd_docs(cases, output_dir):
+    counter = 0
+    num_cases = len(cases)
+    
+    # Remove cases that don't have final written decisions
+    fwd_cases = []
+    for case in cases:
+        trial_id = case["trialNumber"].upper()
+        doc_filename = path.join(output_dir, "{}.pdf".format(trial_id))
+        if case["prosecutionStatus"] == "FWD Entered" and not path.isfile(doc_filename):
+            fwd_cases.append(case)
+    num_cases = len(fwd_cases)
+    print("Found {} cases with final decisions to be downloaded".format(num_cases))
+    
+    for case in fwd_cases:
+        counter += 1
+        trial_id = case["trialNumber"].upper()
+        print("[{}/{}] Processing {}".format(counter, num_cases, trial_id))
+        doc_filename = path.join(output_dir, "{}.pdf".format(trial_id))
+        
+        # Find link to this case's documents
+        docs = None
+        for case_link in case["links"]:
+            if case_link["rel"] == "documents":
+                docs = get_json_at(case_link["href"])
+                break
+        random_sleep()
+        
+        # Find the document that has the final written decision
+        if docs is None:
+            print("Could not fetch document metadata for {}".format(trial_id))
+            continue
+        docs = docs["results"]
+        fwd_doc_link = None
+        for doc in docs:
+            if fwd_doc_link is not None:
+                break
+            if doc["type"].lower() == "final decision" \
+                    or contains(doc["title"].lower(), ["final written decision", 
+                                                       "termination decision document"]):
+                if "expunged" in doc["title"].lower():
+                    continue
+                for doc_link in doc["links"]:
+                    if doc_link["rel"] == "download":
+                        fwd_doc_link = doc_link["href"]
+                        break
+        
+        # Download and save the decision document
+        if fwd_doc_link is None:
+            print("Could not find decision document for {}".format(trial_id))
+            continue
+        try:
+            doc_contents = get_raw_contents_at(fwd_doc_link)
+        except urllib2.HTTPError as err:
+            print(err)
+            continue
+        
+        with open(doc_filename, "w") as fd:
+            fd.write(doc_contents)
+        random_sleep()
+
+
+def main():
+    argv = sys.argv[1:]
+    if len(argv) != 2:
+        print("Usage: ptab_api_downloader.py path/to/case_data_file path/to/fwd_docs")
+        sys.exit(1)
+    
+    casedata_filename = argv[0]
+    output_dir = argv[1]
+    # Create output directory to hold decision documents if it doesn't exist
+    if not path.exists(output_dir):
+        makedirs(output_dir)
+    
+    cases = get_case_metadata()
     
     # Write case information as JSON to output file
-    json.dump(cases, out_fd, indent=2)
-    out_fd.close()
-     
+    casedata_fd = open(casedata_filename, "w")
+    json.dump(cases, casedata_fd, indent=2)
+    casedata_fd.close()
+    
+    # Download decision documents for cases with final written decisions
+    download_fwd_docs(cases, output_dir)
+
 
 if __name__ == "__main__":
     main()
