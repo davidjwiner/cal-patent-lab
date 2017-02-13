defcdd506fe6ccb4b76d8922d18f577a4e5bae87
William Ho <william.zt.ho@berkeley.edu>
2017-02-12 18:25:55 -0800

    Extend, rename au_examiner_api_downloader.py

--- lib/pair_api_downloader.py
+++ lib/pair_api_downloader.py
@@ -1,6 +1,7 @@
+import csv
+from dateutil.parser import parse
 import json
 import requests
-import csv
 import sys
 
 # calling the api to get the examiner name and art unit
@@ -19,15 +20,19 @@ def get_au_examiner(patentId):
     # trying to read information from the result
     # if not available just return the patent id
     try:
-        examiner_name = result['queryResults']["searchResponse"][
-            'response']["docs"][0]["appExamNameFacet"]
-        art_unit = result['queryResults']["searchResponse"][
-                'response']["docs"][0]['appGrpArtNumber']
+        result = result['queryResults']["searchResponse"][
+            'response']["docs"][0]
+        examiner_name = result["appExamNameFacet"]
+        art_unit      = result['appGrpArtNumber']
+        filing_date   = parse(result["appFilingDate"]).strftime("%Y-%m-%d")
+        issue_date    = parse(result["patentIssueDate"]).strftime("%Y-%m-%d")
     except (IndexError, KeyError):
         print(patentId)
-        examiner_name = 'None'
-        art_unit = 'None'
-    return  examiner_name, art_unit
+        examiner_name = None
+        art_unit      = None
+        filing_date   = None
+        issue_date    = None
+    return examiner_name, art_unit, filing_date, issue_date
 
 
 # create a csv file containing: patent_id, art unit, examiner name
@@ -36,26 +41,27 @@ def create_csv(ptab_api_filename, output_filename):
     api_cases = json.load(open(ptab_api_filename))
     au_examiner_file = open(output_filename, "w")
     p = csv.writer(au_examiner_file)
-    p.writerow(["patentId", "Art Unit", "Examiner Name"])
+    p.writerow(["patentId", "artUnit", "examinerName", "filingDate", "issueDate"])
     count = 0
+    num_cases = len(api_cases)
     for case in api_cases:
         count += 1
         try:
             patent_id = case['patentNumber']
         except KeyError:
             print(case)
-        examiner, au = get_au_examiner(patent_id)
-        p.writerow([patent_id, au, examiner])
-        if count % 100 == 0:
-            print('Written {} cases in csv file'.format(count))
+        examiner, au, filing_date, issue_date = get_au_examiner(patent_id)
+        p.writerow([patent_id, au, examiner, filing_date, issue_date])
+        if count % 10 == 0:
+            print('Written {}/{} cases in csv file'.format(count, num_cases))
     
 
 if __name__ == '__main__':
     argv = sys.argv[1:]
     if len(argv) != 2:
-        print("Usage: au_api_downloader.py ptab_api_metadata_file "
+        print("Usage: pair_api_downloader.py ptab_api_metadata_file "
             "outputfile name")
         sys.exit(1)
     api_data_filename = argv[0]
-    output_filename = argv[1]
+    output_filename   = argv[1]
     create_csv(api_data_filename, output_filename)
