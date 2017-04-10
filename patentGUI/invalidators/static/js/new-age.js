b39c544f30516708a42377c3b4d34bc7347b33e5
dlwndghk94 <dlwndghk94@berkeley.edu>
2017-04-10 15:20:14 -0700

    pictures and descriptions

--- patentGUI/invalidators/static/js/new-age.js
+++ patentGUI/invalidators/static/js/new-age.js
@@ -28,4 +28,9 @@
         }
     })
 
+    $("#pop").on("click", function() {
+   $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
+   $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+});
+
 })(jQuery); // End of use strict
