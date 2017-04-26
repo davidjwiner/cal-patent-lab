94908a02a34e81c8f6d647af5e7a0bdae7827b22
David Winer <davidjwiner@gmail.com>
2017-04-25 22:39:20 -0700

    Update .gitignore with config file

--- lib/notebooks/denials_algorithm/denials_config.py
+++ lib/notebooks/denials_algorithm/denials_config.py
@@ -1,5 +1,5 @@
-team_username = ***REMOVED***
-team_password = ***REMOVED***
+team_username = "***REMOVED***"
+team_password = "***REMOVED***"
 
 fung_username = "uspto"
-fung_password = ***REMOVED***
+fung_password = "ferrisbueller"
